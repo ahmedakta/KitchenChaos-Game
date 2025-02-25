c377accd219fa14a449f527127fdc08962d10bf6
ahmedakta <ahmdekta@gmail.com>
2025-02-25 23:25:41 +0300

    interact functions

--- Assets/scripts/GameInput.cs
+++ Assets/scripts/GameInput.cs
@@ -8,6 +8,12 @@ public class GameInput : MonoBehaviour
    private void Awake(){
       playerInputActions = new PlayerInputActions();
       playerInputActions.Player.Enable();
+
+      playerInputActions.Player.Interact.performed += Interact_performed;
+   }
+
+   private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj){
+    Debug.Log(obj);
    }
 
     // Start is called before the first frame update
